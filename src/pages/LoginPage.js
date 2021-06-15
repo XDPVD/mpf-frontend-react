818af5a164b20c62dfcff880e8f6872dca069433
xdpvd@hotmail.es <xdpvd@hotmail.es>
2021-06-15 07:50:36 -0500

    Structure File (Pages and components)

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPage() {
+    return (
+        <>
+            Login Page
+        </>
+    )
+}
+
+export default LoginPage
---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterPage() {
+    return (
+        <>
+            Register Page
+        </>
+    )
+}
+
+export default RegisterPage

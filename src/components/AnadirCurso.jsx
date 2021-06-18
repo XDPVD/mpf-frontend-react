729ff7e8f0040a63dc517dca64f2d6e8563ce3c6
Cristian-Aquino <yojar.aquino@unmsm.edu.pe>
2021-06-18 00:43:02 -0500

    modal_ver

--- src/components/AnadirCurso.jsx
+++ src/components/AnadirCurso.jsx
@@ -1,60 +1,76 @@
-import React from 'react'
-import { Card, CardActions, CardContent, Typography  } from '@material-ui/core'
+import React,{useState} from 'react'
+import { Card, CardActions, CardContent, Typography, CardMedia  } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
-import { display } from '@material-ui/system';
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-
-import { useState } from "react";
-import styled from "styled-components";
+import { Modal } from '@material-ui/core';
 import { Icon } from '@material-ui/core';
-const MyTab = styled(Tab)`
-  text-transform: none;
-  min-width: 80px;
-  margin: 0 5px;
-`;
-const AnadirCurso = (props) => {
-    const [selectedTab, setSelectedTab] = useState(0);
 
-    const handleChange = (event, newValue) => {
-        setSelectedTab(newValue);
-    };
-
+const Carman = (props) => {
     const {classes} = props
+    
+    const [modal, setModal] = useState(false);
+
+    const handleOpen = () => {
+        setModal(!modal);
+    };
+    const body = (
+        <div align='center' className={classes.ventana}>
+            <label className={classes.tile}>MATERIAL NOMBRE</label>
+            <hr></hr>
+            <p align='left'>Proin facilisis dignissim lacus, sit amet vestibulum quam varius vitae. Praesent sit amet nibh nisl. Duis augue magna, consequat et ultricies id, euismod quis nulla. 
+                Vestibulum pulvinar elit eu velit hendrerit, non aliquam elit sodales. Quisque vel condimentum mi, in sagittis tellus. Nulla at nunc ut est sagittis euismod. 
+                Nunc hendrerit tristique eros in aliquet.
+            </p>
+            
+            <Card display ='flex'>
+                <div className={classes.conten}>
+                    <CardMedia width='151px'>
+                        <Icon className={classes.iconCon}>
+                            assignment
+                        </Icon>
+                    </CardMedia>
+                    <div>
+                        <CardContent flex='1 0 auto'>
+                            <Typography className={classes.tile} component="h5" variant="h5">
+                                BASE DE DATOS 2
+                            </Typography>
+                        </CardContent>
+                    </div>
+                </div>    
+            </Card>
+                    
+            <div className={classes.conten}>
+                <Button className={classes.btn} size="large" variant="contained" color="secondary" onClick={()=>handleOpen()}>SALIR</Button>
+            </div>
+        </div>
+      );
 
     return ( 
         <>
-        <Typography variant="h4">Métodos Formales para Pruebas</Typography>
-        <Tabs value={selectedTab} onChange={handleChange}>
-            <MyTab label="Dashboard" />
-            <MyTab label="Clases" />
-            <MyTab label="Tareas" />
-            <MyTab label="Exámenes" />
-            <MyTab label="Personas" />
-        </Tabs>
         <Card className={classes.prin}>
             <div className={classes.conten}>
                 <Icon className={classes.iconCon}>
                     assignment
                 </Icon>
                 <CardContent >
-                    <Typography className={classes.tile}>Material</Typography>
+                    <Typography className={classes.tile}>MATERIAL</Typography>
                 </CardContent>
                 <CardActions>
-                    <Button className={classes.btn} size="large" variant="contained" color="primary">SUBIR</Button>
+                        <Button className={classes.btn} size="large" variant="contained" color="primary" onClick={()=>handleOpen()}>VER</Button>
                 </CardActions>
             </div>
         </Card>
+        <Modal open={modal} onClose={handleOpen}>
+            {body}
+        </Modal>
         </>
      );
 }
  
-
 export default withStyles({
     prin:{
         display:'flex',
-        margin: '40px 100px 40px 250px',
+        margin: '40px',
         border:'2px solid',
         borderRadius: '10px',
     },
@@ -62,15 +78,32 @@ export default withStyles({
         fontSize: 40,
         margin:'10px',
     },
+    item:{
+        margin:'1em',
+        borde: '1px solid',
+        boxSizing:'border-box',
+    },
     btn:{
-        marginLeft: '800px'
+        margin:'5px',
     },
     conten:{
         display:'flex',
         flexDirection:'row',
     },
+    ventana:{
+        position: 'absolute',
+        width: 800,
+        backgroundColor: 'white',
+        border: '2px solid #000',
+        boxShadow: '10px 5px 5px black',
+        padding: '16px 32px 24px',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%,-50%)',
+        FlexDirection:'column',
+    },
     iconCon:{
         fontSize: 80,
         margin: '10px'
     }
-})(AnadirCurso);
+}) (Carman);

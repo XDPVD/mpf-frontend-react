1eeb25ccf0b680cf76a494d2c34d4fa5cb11429d
xdpvd@hotmail.es <xdpvd@hotmail.es>
2021-06-28 22:37:40 -0500

    changeUrl

--- src/base/settings/axios.js
+++ src/base/settings/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://95f9e6707c54.ngrok.io/"
+  baseURL: "https://95f9e6707c54.ngrok.io/"
 });
 
 export default instance;

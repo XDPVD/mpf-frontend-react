af4ddf1cbbfb33d364a640e6224e3d0d6b2c7bfd
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2021-06-28 22:40:09 -0500

    baseURL updated

--- src/base/settings/axios.js
+++ src/base/settings/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://a61457cab5b0.ngrok.io",
+  baseURL: "https://a61457cab5b0.ngrok.io",
 });
 
 export default instance;

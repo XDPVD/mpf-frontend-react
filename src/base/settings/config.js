96a41066859b78e78a7ec7bdd4da9be65086beeb
warevil <60890246+warevil@users.noreply.github.com>
2021-07-20 23:01:32 -0500

    blockGroup habilitado
    pero no funca xD

--- src/base/settings/config.js
+++ src/base/settings/config.js
@@ -16,29 +16,30 @@ export const courseUrls = {
   personas: "/personas",
 };
 
-export const endP = ({ courseId, userId, pubId, commentId, email, code }) => {
+export const endP = ({ courseId, userId, pubId, commentId, email, code, groupId }) => {
   console.log("endP commentId ", { courseId, userId, pubId, commentId });
   return {
-    getCourses: "/course",
-    createCourse: "/course",
     blockAllGroups: `/group/lock_all/${courseId}`,
+    blockGroup: `/group/lock/${groupId}`,
+    createCourse: "/course",
     createGroup: `/group/create/${courseId}/default`,
+    createUser: "/user",
+    delegateUser: `/course/${courseId}/delegate/${userId}`,
     enrollCourseByMail: `/course/${courseId}/enroll/by_email/${email}`,
     enrollMeByCode: `/course/${code}/enroll_me`,
-    delegateUser: `/course/${courseId}/delegate/${userId}`,
     getInscriptions: `/course/${courseId}/inscriptions`,
     getCourse: `/course/${courseId}`,
+    getCourses: "/course",
     getGroups: `/group/group/${courseId}`,
-    getUsers: "/user",
-    createUser: "/user",
     getUser: `/user/${userId}`,
+    getUsers: "/user",
     getComments: `/comment/publication/${pubId}`,
     getAnswers: `/comment/sub/${commentId}`,
-    postComment: `/publication/${pubId}/comment`,
-    postAnswer: `/comment/${commentId}`,
     postAnnounce: `/publication/exam/${courseId}`,
+    postAnswer: `/comment/${commentId}`,
+    postComment: `/publication/${pubId}/comment`,
+    postExam: `/publication/exam/${courseId}`,
     postMaterial: `/publication/material/${courseId}`,
     postTask: `/publication/assignment/${courseId}`,
-    postExam: `/publication/exam/${courseId}`,
   };
 };
--- src/components/CourseUsers/GroupCard.jsx
+++ src/components/CourseUsers/GroupCard.jsx
@@ -1,17 +1,26 @@
+import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Divider from "@material-ui/core/Divider";
 import UsersList from "./UsersList";
-import Button from "@material-ui/core/Button";
 import { useStyles } from "./_styles.js";
+import { endP } from "@settings/config";
+import { postData } from "@utils/postData";
+
 
 function GroupCard(props) {
   const classes = useStyles();
   const users = props.users;
   const isAdmin = props.isAdmin;
-  const name = props.group?.name
+  const name = props.group?.name;
+  const groupId = props.group?.id;
+
+  async function blockGroup(event) {
+    event.preventDefault();
+    await postData(endP({ groupId }).blockGroup);
+  }
 
   return (
     <Card className={classes.card}>
@@ -27,7 +36,7 @@ function GroupCard(props) {
               backgroundColor='#000000'
               variant='contained'
               color='secondary'
-              // onClick={handleClickOpen}
+              onClick={blockGroup}
             >
               Bloquear grupo
             </Button>

bd54a50214733174042a45954cb56a87003b5e2a
warevil <60890246+warevil@users.noreply.github.com>
2021-08-02 01:05:51 -0500

    Update sonarqube-scanner.js

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -3,13 +3,13 @@ scanner(
   {
   serverUrl: "http://localhost:9000",
   login:"admin",
-  password:"admin",
+  password:"1234",
   options: {
     "sonar.sources": "./src",
-	"sonar.tests": "./src",
-	"sonar.test.inclusions": "**/*.test.jsx,**/*.test.js",
-	"sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
-	"sonar.testExecutionReportPaths": "test-report.xml"
+	  "sonar.tests": "./src",
+	  "sonar.test.inclusions": "**/*.test.jsx,**/*.test.js",
+    "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
+    "sonar.testExecutionReportPaths": "test-report.xml"
   },
 },
 () => process.exit()

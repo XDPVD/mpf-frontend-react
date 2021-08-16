54dd68c535cf0438d8fc326369fe545ee7017bc5
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2021-08-16 16:41:32 -0500

    fixes

--- src/components/CoursesList/__tests__/CoursesCreatedList.test.js
+++ src/components/CoursesList/__tests__/CoursesCreatedList.test.js
@@ -1,32 +1,34 @@
-import { render, screen } from "@testing-library/react";
-import CoursesCreatedList from "../CoursesCreatedList";
-import BaseComponent from "@mocks/BaseComponent";
+import { render, screen } from '@testing-library/react';
+import CoursesCreatedList from '../CoursesCreatedList';
+import BaseComponent from '@mocks/BaseComponent';
 
-const mockData = [
-  {
-    id: 1,
-    name: "Curso 1",
-    description: "Esta es una descripción",
-  },
-  {
-    id: 2,
-    name: "Curso 2",
-    description: "Esta es otra descripción",
-  },
-];
+const mockData = {
+    courses_created: [
+        {
+            id: 1,
+            name: 'Curso 1',
+            description: 'Esta es una descripción',
+        },
+        {
+            id: 2,
+            name: 'Curso 2',
+            description: 'Esta es otra descripción',
+        },
+    ],
+};
 
-describe("CoursesCreatedList", () => {
-  beforeEach(() => {
-    render(
-      <BaseComponent>
-        <CoursesCreatedList created={mockData} />
-      </BaseComponent>
-    );
-  });
+describe('CoursesCreatedList', () => {
+    beforeEach(() => {
+        render(
+            <BaseComponent>
+                <CoursesCreatedList courses={mockData} />
+            </BaseComponent>
+        );
+    });
 
-  it("should render two courses", async () => {
-    const cursos = screen.getAllByTestId(/course-item/i);
+    it('should render two courses', async () => {
+        const cursos = screen.getAllByTestId(/course-item/i);
 
-    expect(cursos.length).toBe(2);
-  });
+        expect(cursos.length).toBe(2);
+    });
 });
